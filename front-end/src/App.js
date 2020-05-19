566c86258433e83be57b76e7f39d71285eb4b3b2
blues-man <nvinto@redhat.com>
2020-05-19 16:10:51 +0000

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Wine from "./Wine";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Wine />
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/Wine.js
@@ -0,0 +1,43 @@
+import React, {Component} from "react";
+
+class Wine extends Component {
+  constructor(props) {
+    super([props]);
+    this.wines = [];
+  }
+
+  componentDidMount() {
+    return fetch("https://backend-rewine-rewine-backend.apps.cluster-nantes-715e.nantes-715e.example.opentlc.com/rewine/backend/wines")
+           .then(resp => resp.json())
+           .then(backend => {
+                console.log(backend);
+                backend.map((p, index) => {
+                let newWine = this.wines;
+                newWine.push({id: index, image: p.wineId, name: p.name, origin: p.origin});
+                
+                this.setState({wines: newWine});
+                });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h1>Wine List</h1>
+
+       {this.wines.map(element => {
+          return (
+        <div class="gallery">
+              <div class="desc">
+                <p>{element.name}</p>
+                <p>{element.origin}</p>
+              </div>
+        </div>
+          )
+        })}
+    </div>
+    )
+  }
+}
+
+export default Wine;
